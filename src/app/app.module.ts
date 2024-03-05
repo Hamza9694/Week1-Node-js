f9c215e722cfe0b0def1928bffbc8549d6df4614
Muhammad Hamza Suleman <hamzasuleman687@gmail.com>
2024-03-05 12:58:21 +0500

    Add Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     
-    MessagesComponent
+    MessagesComponent,
+         DashboardComponent
   ],
   imports: [
     BrowserModule,
